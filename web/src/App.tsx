2bb17a87cce79866cefef3e8fd9a723d1ade2af6
G-PPeralta <prates_peralta@hotmail.com>
2023-01-17 19:15:40 -0300

    feat: style Habit component with tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import { Fragment } from "react";
 import { Habit } from "./components/Habit";
 
+import "./styles/global.css";
+
 function App() {
   return (
     <Fragment>
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
+      <Habit completed={3} />
       <Habit completed={3} />
     </Fragment>
   );
