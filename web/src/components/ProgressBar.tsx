623f90a34eeb2e325de376cfdf2c990db8cf95a4
G-PPeralta <prates_peralta@hotmail.com>
2023-01-19 17:18:05 -0300

    feat: create ProgressBar component

---
+++ web/src/components/ProgressBar.tsx
@@ -0,0 +1,19 @@
+interface ProgressBarProps {
+  progress: number;
+}
+
+export function ProgressBar(props: ProgressBarProps) {
+  return (
+    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+      <div
+        role="progressbar"
+        className="h-3 rounded-xl bg-violet-600"
+        aria-label="Progresso de Hábitos Completados Nesse dia"
+        aria-valuenow={props.progress}
+        style={{
+          width: `${props.progress}%`,
+        }}
+      ></div>
+    </div>
+  );
+}
