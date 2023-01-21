c7eee89a5d0b0ea293cf812bdfd52fea7158be2a
G-PPeralta <prates_peralta@hotmail.com>
2023-01-21 10:39:54 -0300

    feat: implements comunication between habitsList and ProgressBar

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,23 +1,34 @@
+import { useState } from "react";
+
 import { ProgressBar } from "./ProgressBar";
+import { HabitsList } from "./HabitsList";
 
 import * as Popover from "@radix-ui/react-popover";
-
 import dayjs from "dayjs";
 import clsx from "clsx";
-import { HabitsList } from "./HabitsList";
 
 interface HabitDayProps {
   date: Date;
-  completed?: number;
+  defaultCompleted?: number;
   amount?: number;
 }
 
-export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
+export function HabitDay({
+  defaultCompleted = 0,
+  amount = 0,
+  date,
+}: HabitDayProps) {
+  const [completed, setCompleted] = useState(defaultCompleted);
+
   const completedPercentage =
     amount > 0 ? Math.round((completed / amount) * 100) : 0;
 
   const dayAndMonth = dayjs(date).format("DD/MM");
   const dayOfWeek = dayjs(date).format("ddd");
+
+  function handleCompletedChange(completed: number) {
+    setCompleted(completed);
+  }
 
   return (
     <Popover.Root>
@@ -45,7 +56,7 @@ export function HabitDay({ completed = 0, amount = 0, date }: HabitDayProps) {
 
           <ProgressBar progress={completedPercentage} />
 
-          <HabitsList date={date} />
+          <HabitsList date={date} onCompletedChange={handleCompletedChange} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
 
-import { Check } from "phosphor-react";
 import * as Checkbox from "@radix-ui/react-checkbox";
+import dayjs from "dayjs";
+
+import { Check } from "phosphor-react";
 import { api } from "../lib/axios";
 
 interface HabitsListProps {
   date: Date;
+  onCompletedChange: (completed: number) => void;
 }
 
 interface HabitsInfo {
@@ -17,7 +20,7 @@ interface HabitsInfo {
   completedHabits: string[];
 }
 
-export function HabitsList({ date }: HabitsListProps) {
+export function HabitsList({ date, onCompletedChange }: HabitsListProps) {
   const [habitsInfo, setHabitsInfo] = useState<HabitsInfo>();
 
   useEffect(() => {
@@ -30,13 +33,42 @@ export function HabitsList({ date }: HabitsListProps) {
       .then((response) => setHabitsInfo(response.data));
   }, []);
 
+  const isDateInPast = dayjs(date).endOf("day").isBefore(new Date());
+
+  async function handleToggleHabit(habitId: string) {
+    await api.patch(`/habits/${habitId}/toggle`);
+
+    let completedHabits: string[] = [];
+
+    const isHabitAlreadyCompleted =
+      habitsInfo?.completedHabits.includes(habitId);
+
+    if (isHabitAlreadyCompleted) {
+      completedHabits = habitsInfo!.completedHabits.filter(
+        (id) => id !== habitId
+      );
+    } else {
+      completedHabits = [...habitsInfo!.completedHabits, habitId];
+    }
+
+    setHabitsInfo({
+      possibleHabits: habitsInfo!.possibleHabits,
+      completedHabits,
+    });
+
+    onCompletedChange(completedHabits.length);
+  }
+
   return (
     <div className="mt-6 flex flex-col gap-3">
       {habitsInfo?.possibleHabits.map((habit) => {
         return (
           <Checkbox.Root
             key={habit.id}
+            onCheckedChange={() => handleToggleHabit(habit.id)}
             className="flex items-center gap-3 group"
+            checked={habitsInfo.completedHabits.includes(habit.id)}
+            disabled={isDateInPast}
           >
             <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
               <Checkbox.Indicator>
